e657ca6b1def1477054aa65725717af287d7d4fd
JP <jp@jp.nl>
2021-12-07 11:41:02 +0100

    Formulieren met template-driven en model-driven aanpak laten zien

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import {Driver } from './models/driver';
 
 @Component({
@@ -7,9 +8,12 @@ import {Driver } from './models/driver';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'JP';
-
-  bedrag = 1234567.89;
+	addDriverForm = new FormGroup({
+		name: new FormControl(undefined, [Validators.required, Validators.pattern('^[a-zA-Z -]+$')]),
+		points: new FormControl(undefined, Validators.required),
+		photoUrl: new FormControl(undefined, Validators.required),
+	});
+	newDriver = {} as Driver;
 
 	drivers: Driver[] = [
 		{ id: 4, name: 'Maxie Verstappen', points: 369.5, photoUrl: 'https://cdn.nos.nl/image/2021/12/04/809027/1024x576a.jpg'},
@@ -17,16 +21,11 @@ export class AppComponent {
 		{ id: 15, name: 'Valttori Bottas', points: 312, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/14/F12019_Schloss_Gabelhofen_%2820%29.jpg'},
 	];
 
-  changeName() {
-	  this.name += 'Paul';
-  }
+	addDriverReactive() {
+		this.drivers.push(this.addDriverForm.value);
+	}
 
-  addDriver() {
-	  this.drivers.push({
-		  id: 16,
-		  name: 'Checo Perez',
-		  points: 210,
-		  photoUrl: 'https://images0.persgroep.net/rcs/ccLyZZfc0iWpy2PiB_LmawXZ-9I/diocontent/210640088/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8'
-	  });
-  }
+	addDriverTemplate() {
+		this.drivers.push(this.newDriver);
+	}
 }
--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -12,7 +12,8 @@ import { LewisLoserPipe } from './pipes/lewis-loser.pipe';
   ],
   imports: [ // modules
     BrowserModule,
-	FormsModule
+	FormsModule,
+	ReactiveFormsModule
   ],
   providers: [  ], // globale instellingen & services voor DI
   bootstrap: [AppComponent]
